0f8f8db44f273a876c8fbb4a90b452cfe5e27395
tajimyradow <tajimyradow12345@gmail.com>
2024-12-13 13:09:46 +0500

    [fix]: students faculty and department names in GetStudents

--- internal/repositories/admin/student.go
+++ internal/repositories/admin/student.go
@@ -161,16 +161,16 @@ func (s *StudentRepository) GetStudents(input models.StudentSearch) (models.Stud
 				   s.group_id,
 				   s.birth_date,
 				   coalesce(s.image, '')    as image,
-				   f.id,
-				   f.name,
-				   d.id,
-				   d.name,
+				   f.id as faculty_id,
+				   f.name as faculty_name,
+				   d.id as department_id,
+				   d.name as department_name,
 				   g.name                   as group_name
 			from students as s
 					 join groups as g on g.id = s.group_id
 					join professions as p on g.profession_id = p.id
 			join departments as d on p.department_id = d.id
-			join faculties as f on d.faculty_id = f.id 
+			join faculties as f on d.faculty_id = f.id
 			where ` + queryArgs
 	} else {
 		query = `
@@ -184,10 +184,10 @@ func (s *StudentRepository) GetStudents(input models.StudentSearch) (models.Stud
 				   s.group_id,
 				   s.birth_date,
 				   coalesce(s.image, '')    as image,
-				   f.id,
-				   f.name,
-				   d.id,
-				   d.name,
+				   f.id as faculty_id,
+				   f.name as faculty_name,
+				   d.id as department_id,
+				   d.name as department_name,
 				   g.name                   as group_name
 			from students as s
 					 join groups as g on g.id = s.group_id
